9ac8a0bc93e6b0fef63ee764d1382d406d17151f
Abul Kalam Azad <azad@transvelo.com>
2022-06-03 14:34:20 +0530

    typed headline

--- templates/widgets/multipurpose-text.php
+++ templates/widgets/multipurpose-text.php
@@ -11,6 +11,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 $settings = $widget->get_settings_for_display();
 
+$animation_typed_text_array = explode( "\n", $settings[ 'typing_text' ] );
+
+$animation_typed_text = array(
+			'strings' => $animation_typed_text_array
+		);
+
 if ( '' === $settings['highlighted_text'] && '' === $settings['before_title'] ) {
 	return;
 }
