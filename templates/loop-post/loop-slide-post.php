0e983bde7a99cfad4076146bb26af6b833b38f7c
Abul Kalam Azad <azad@Transvelo-AbulKalamAzad.local>
2022-06-22 13:44:27 +0530

    #9

---
+++ templates/loop-post/loop-slide-post.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Template for displaying loop slide post.
+ *
+ * @package MASElementor/Templates/loop-post/loop-slide-post.php
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?>
+<!-- Slide -->
+<div class="swiper-slide pt-4 pb-8">
+    <a class="card card-transition" href="#">
+        <div class="card-pinned">
+            <img class="card-img-top" src="./assets/svg/components/card-3.svg" alt="Image Description">
+            <span class="badge bg-dark text-white card-pinned-top-end">Design</span>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Explore the Snippets tool</h5>
+            <p>Answer a few simple questions to get your policy. Talk to an agent if you need help.</p>
+        </div>
+        <div class="card-footer pt-0">
+            <span class="card-link">Read more</span>
+        </div>
+    </a>
+
+    <a class="card card-transition bg-primary" href="#" style="background-image: url(./assets/svg/components/wave-pattern-light.svg);">
+        <div class="card-body">
+            <span class="card-subtitle text-white mb-3">Product</span>
+            <h3 class="card-title text-white lh-base">Assessing Constraints: Making Products for all Users</h3>
+        </div>
+        <div class="card-footer pt-0">
+            <span class="card-link link-light">Read more</span>
+        </div>
+    </a>
+</div>
+<!-- End Slide -->
+    
