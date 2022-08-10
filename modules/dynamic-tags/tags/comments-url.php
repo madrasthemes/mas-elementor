50fe9709a377aff5138ee3e01f4891cb14892bcb
Abul Kalam Azad <azad@Transvelo-AbulKalamAzad.local>
2022-08-10 15:54:00 +0530

    #comments-dynamic-tag

---
+++ modules/dynamic-tags/tags/comments-url.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Comments URL.
+ *
+ * @package MASElementor\Modules\DynamicTags\tags\comments-url.php
+ */
+
+namespace MASElementor\Modules\DynamicTags\Tags;
+
+use MASElementor\Modules\DynamicTags\Tags\Base\Data_Tag;
+use MASElementor\Modules\DynamicTags\Module;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+/**
+ * Comments URL class.
+ */
+class Comments_URL extends \Elementor\Core\DynamicTags\Data_Tag {
+	/**
+	 * Get name.
+	 */
+	public function get_name() {
+		return 'comments-url';
+	}
+	/**
+	 * Get the title.
+	 */
+	public function get_title() {
+		return esc_html__( 'Comments URL', 'mas-elementor' );
+	}
+	/**
+	 * Get the group.
+	 */
+	public function get_group() {
+		return Module::COMMENTS_GROUP;
+	}
+	/**
+	 * Get the categories.
+	 */
+	public function get_categories() {
+		return array( Module::URL_CATEGORY );
+	}
+	/**
+	 * Get value.
+	 *
+	 * @param array $options control opions.
+	 */
+	public function get_value( array $options = array() ) {
+		return get_comments_link();
+	}
+}
