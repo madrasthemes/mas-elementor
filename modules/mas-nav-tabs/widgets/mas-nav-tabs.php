50e57a5728892bac46e225998cff69f334b74dd5
Abul Kalam Azad <azad@Transvelo-AbulKalamAzad.local>
2022-08-23 12:10:44 +0530

    #Nav-Tabs

--- modules/mas-nav-tabs/widgets/mas-nav-tabs.php
+++ modules/mas-nav-tabs/widgets/mas-nav-tabs.php
@@ -21,8 +21,9 @@ use MASElementor\Modules\NavTabs\Skins;
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
 /**
- * Nav Tabs Silicon Elementor Widget.
+ * MAS Nav Tabs Elementor Widget.
  */
 class Mas_Nav_Tabs extends Base_Widget {
 
@@ -169,6 +170,22 @@ class Mas_Nav_Tabs extends Base_Widget {
 			)
 		);
 
+		$repeater->add_control(
+			'description_text',
+			array(
+				'label'       => esc_html__( 'Desciption', 'mas-elementor' ),
+				'type'        => Controls_Manager::TEXTAREA,
+				'dynamic'     => array(
+					'active' => true,
+				),
+				'default'     => esc_html__( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.', 'mas-elementor' ),
+				'placeholder' => esc_html__( 'Enter your description', 'mas-elementor' ),
+				'rows'        => 10,
+				'separator'   => 'none',
+				'show_label'  => true,
+			)
+		);
+
 		$this->add_control(
 			'nav_tabs',
 			array(
@@ -177,16 +194,10 @@ class Mas_Nav_Tabs extends Base_Widget {
 				'fields'      => $repeater->get_controls(),
 				'default'     => array(
 					array(
-						'title' => esc_html__( 'Project management', 'mas-elementor' ),
+						'title' => esc_html__( 'Featured Jobs', 'mas-elementor' ),
 					),
 					array(
-						'title' => esc_html__( 'Remote work', 'mas-elementor' ),
-					),
-					array(
-						'title' => esc_html__( 'Product release', 'mas-elementor' ),
-					),
-					array(
-						'title' => esc_html__( 'Campaign planning', 'mas-elementor' ),
+						'title' => esc_html__( 'Recent Jobs', 'mas-elementor' ),
 					),
 				),
 				'title_field' => '{{{ list }}}',
@@ -195,10 +206,302 @@ class Mas_Nav_Tabs extends Base_Widget {
 
 		$this->end_controls_section();
 
+		// Style Tab Controls.
 		$this->start_controls_section(
-			'section_list_style',
+			'section_icon_list',
 			array(
 				'label' => esc_html__( 'List', 'mas-elementor' ),
+				'tab'   => Controls_Manager::TAB_STYLE,
+			)
+		);
+
+		$this->add_control(
+			'ul_wrap',
+			array(
+				'label'       => esc_html__( 'List Wrap Class', 'mas-elementor' ),
+				'type'        => Controls_Manager::TEXT,
+				'placeholder' => esc_html__( 'Enter Your Wrap Class', 'mas-elementor' ),
+			)
+		);
+
+		$this->add_control(
+			'li_wrap',
+			array(
+				'label'       => esc_html__( 'List item Wrap Class', 'mas-elementor' ),
+				'type'        => Controls_Manager::TEXT,
+				'placeholder' => esc_html__( 'Enter Your li Wrap Class', 'mas-elementor' ),
+			)
+		);
+
+		$this->add_responsive_control(
+			'space_between',
+			array(
+				'label'     => esc_html__( 'Space Between', 'mas-elementor' ),
+				'type'      => Controls_Manager::SLIDER,
+				'range'     => array(
+					'px' => array(
+						'max' => 50,
+					),
+				),
+				'selectors' => array(
+					'{{WRAPPER}} .elementor-icon-list-items:not(.elementor-inline-items) .elementor-icon-list-item:not(:last-child)' => 'padding-bottom: calc({{SIZE}}{{UNIT}}/2)',
+					'{{WRAPPER}} .elementor-icon-list-items:not(.elementor-inline-items) .elementor-icon-list-item:not(:first-child)' => 'margin-top: calc({{SIZE}}{{UNIT}}/2)',
+					'{{WRAPPER}} .elementor-icon-list-items.elementor-inline-items .elementor-icon-list-item' => 'margin-right: calc({{SIZE}}{{UNIT}}/2); margin-left: calc({{SIZE}}{{UNIT}}/2)',
+					'{{WRAPPER}} .elementor-icon-list-items.elementor-inline-items' => 'margin-right: calc(-{{SIZE}}{{UNIT}}/2); margin-left: calc(-{{SIZE}}{{UNIT}}/2)',
+					'body.rtl {{WRAPPER}} .elementor-icon-list-items.elementor-inline-items .elementor-icon-list-item:after' => 'left: calc(-{{SIZE}}{{UNIT}}/2)',
+					'body:not(.rtl) {{WRAPPER}} .elementor-icon-list-items.elementor-inline-items .elementor-icon-list-item:after' => 'right: calc(-{{SIZE}}{{UNIT}}/2)',
+				),
+			)
+		);
+
+		$this->add_responsive_control(
+			'icon_align',
+			array(
+				'label'        => esc_html__( 'Alignment', 'mas-elementor' ),
+				'type'         => Controls_Manager::CHOOSE,
+				'options'      => array(
+					'left'   => array(
+						'title' => esc_html__( 'Left', 'mas-elementor' ),
+						'icon'  => 'eicon-h-align-left',
+					),
+					'center' => array(
+						'title' => esc_html__( 'Center', 'mas-elementor' ),
+						'icon'  => 'eicon-h-align-center',
+					),
+					'right'  => array(
+						'title' => esc_html__( 'Right', 'mas-elementor' ),
+						'icon'  => 'eicon-h-align-right',
+					),
+				),
+				'prefix_class' => 'elementor%s-align-',
+			)
+		);
+
+		$this->add_control(
+			'divider',
+			array(
+				'label'     => esc_html__( 'Divider', 'mas-elementor' ),
+				'type'      => Controls_Manager::SWITCHER,
+				'label_off' => esc_html__( 'Off', 'mas-elementor' ),
+				'label_on'  => esc_html__( 'On', 'mas-elementor' ),
+				'selectors' => array(
+					'{{WRAPPER}} .elementor-icon-list-item:not(:last-child):after' => 'content: ""',
+				),
+				'separator' => 'before',
+			)
+		);
+
+		$this->add_control(
+			'divider_style',
+			array(
+				'label'     => esc_html__( 'Style', 'mas-elementor' ),
+				'type'      => Controls_Manager::SELECT,
+				'options'   => array(
+					'solid'  => esc_html__( 'Solid', 'mas-elementor' ),
+					'double' => esc_html__( 'Double', 'mas-elementor' ),
+					'dotted' => esc_html__( 'Dotted', 'mas-elementor' ),
+					'dashed' => esc_html__( 'Dashed', 'mas-elementor' ),
+				),
+				'default'   => 'solid',
+				'condition' => array(
+					'divider' => 'yes',
+				),
+				'selectors' => array(
+					'{{WRAPPER}} .elementor-icon-list-items:not(.elementor-inline-items) .elementor-icon-list-item:not(:last-child):after' => 'border-top-style: {{VALUE}}',
+					'{{WRAPPER}} .elementor-icon-list-items.elementor-inline-items .elementor-icon-list-item:not(:last-child):after' => 'border-left-style: {{VALUE}}',
+				),
+			)
+		);
+
+		$this->add_control(
+			'divider_weight',
+			array(
+				'label'     => esc_html__( 'Weight', 'mas-elementor' ),
+				'type'      => Controls_Manager::SLIDER,
+				'default'   => array(
+					'size' => 1,
+				),
+				'range'     => array(
+					'px' => array(
+						'min' => 1,
+						'max' => 20,
+					),
+				),
+				'condition' => array(
+					'divider' => 'yes',
+				),
+				'selectors' => array(
+					'{{WRAPPER}} .elementor-icon-list-items:not(.elementor-inline-items) .elementor-icon-list-item:not(:last-child):after' => 'border-top-width: {{SIZE}}{{UNIT}}',
+					'{{WRAPPER}} .elementor-inline-items .elementor-icon-list-item:not(:last-child):after' => 'border-left-width: {{SIZE}}{{UNIT}}',
+				),
+			)
+		);
+
+		$this->add_control(
+			'divider_width',
+			array(
+				'label'     => esc_html__( 'Width', 'mas-elementor' ),
+				'type'      => Controls_Manager::SLIDER,
+				'default'   => array(
+					'unit' => '%',
+				),
+				'condition' => array(
+					'divider' => 'yes',
+					'view!'   => 'inline',
+				),
+				'selectors' => array(
+					'{{WRAPPER}} .elementor-icon-list-item:not(:last-child):after' => 'width: {{SIZE}}{{UNIT}}',
+				),
+			)
+		);
+
+		$this->add_control(
+			'divider_height',
+			array(
+				'label'      => esc_html__( 'Height', 'mas-elementor' ),
+				'type'       => Controls_Manager::SLIDER,
+				'size_units' => array( '%', 'px' ),
+				'default'    => array(
+					'unit' => '%',
+				),
+				'range'      => array(
+					'px' => array(
+						'min' => 1,
+						'max' => 100,
+					),
+					'%'  => array(
+						'min' => 1,
+						'max' => 100,
+					),
+				),
+				'condition'  => array(
+					'divider' => 'yes',
+					'view'    => 'inline',
+				),
+				'selectors'  => array(
+					'{{WRAPPER}} .elementor-icon-list-item:not(:last-child):after' => 'height: {{SIZE}}{{UNIT}}',
+				),
+			)
+		);
+
+		$this->add_control(
+			'divider_color',
+			array(
+				'label'     => esc_html__( 'Color', 'mas-elementor' ),
+				'type'      => Controls_Manager::COLOR,
+				'default'   => '#ddd',
+				'global'    => array(
+					'default' => Global_Colors::COLOR_TEXT,
+				),
+				'condition' => array(
+					'divider' => 'yes',
+				),
+				'selectors' => array(
+					'{{WRAPPER}} .elementor-icon-list-item:not(:last-child):after' => 'border-color: {{VALUE}}',
+				),
+			)
+		);
+
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'section_icon_style',
+			array(
+				'label' => esc_html__( 'Icon', 'mas-elementor' ),
+				'tab'   => Controls_Manager::TAB_STYLE,
+			)
+		);
+
+		$this->add_control(
+			'icon_color',
+			array(
+				'label'     => esc_html__( 'Color', 'mas-elementor' ),
+				'type'      => Controls_Manager::COLOR,
+				'default'   => '',
+				'selectors' => array(
+					'{{WRAPPER}} .elementor-icon-list-icon i' => 'color: {{VALUE}};',
+					'{{WRAPPER}} .elementor-icon-list-icon svg' => 'fill: {{VALUE}};',
+				),
+				'global'    => array(
+					'default' => Global_Colors::COLOR_PRIMARY,
+				),
+			)
+		);
+
+		$this->add_control(
+			'icon_color_hover',
+			array(
+				'label'     => esc_html__( 'Hover', 'mas-elementor' ),
+				'type'      => Controls_Manager::COLOR,
+				'default'   => '',
+				'selectors' => array(
+					'{{WRAPPER}} .elementor-icon-list-item:hover .elementor-icon-list-icon i' => 'color: {{VALUE}};',
+					'{{WRAPPER}} .elementor-icon-list-item:hover .elementor-icon-list-icon svg' => 'fill: {{VALUE}};',
+				),
+			)
+		);
+
+		$this->add_responsive_control(
+			'icon_size',
+			array(
+				'label'     => esc_html__( 'Size', 'mas-elementor' ),
+				'type'      => Controls_Manager::SLIDER,
+				'default'   => array(
+					'size' => 14,
+				),
+				'range'     => array(
+					'px' => array(
+						'min' => 6,
+					),
+				),
+				'selectors' => array(
+					'{{WRAPPER}}' => '--e-icon-list-icon-size: {{SIZE}}{{UNIT}};',
+				),
+			)
+		);
+
+		$e_icon_list_icon_css_var      = 'var(--e-icon-list-icon-size, 1em)';
+		$e_icon_list_icon_align_left   = sprintf( '0 calc(%s * 0.25) 0 0', $e_icon_list_icon_css_var );
+		$e_icon_list_icon_align_center = sprintf( '0 calc(%s * 0.125)', $e_icon_list_icon_css_var );
+		$e_icon_list_icon_align_right  = sprintf( '0 0 0 calc(%s * 0.25)', $e_icon_list_icon_css_var );
+
+		$this->add_responsive_control(
+			'icon_self_align',
+			array(
+				'label'                => esc_html__( 'Alignment', 'mas-elementor' ),
+				'type'                 => Controls_Manager::CHOOSE,
+				'options'              => array(
+					'left'   => array(
+						'title' => esc_html__( 'Left', 'mas-elementor' ),
+						'icon'  => 'eicon-h-align-left',
+					),
+					'center' => array(
+						'title' => esc_html__( 'Center', 'mas-elementor' ),
+						'icon'  => 'eicon-h-align-center',
+					),
+					'right'  => array(
+						'title' => esc_html__( 'Right', 'mas-elementor' ),
+						'icon'  => 'eicon-h-align-right',
+					),
+				),
+				'default'              => '',
+				'selectors_dictionary' => array(
+					'left'   => sprintf( '--e-icon-list-icon-align: left; --e-icon-list-icon-margin: %s;', $e_icon_list_icon_align_left ),
+					'center' => sprintf( '--e-icon-list-icon-align: center; --e-icon-list-icon-margin: %s;', $e_icon_list_icon_align_center ),
+					'right'  => sprintf( '--e-icon-list-icon-align: right; --e-icon-list-icon-margin: %s;', $e_icon_list_icon_align_right ),
+				),
+				'selectors'            => array(
+					'{{WRAPPER}}' => '{{VALUE}}',
+				),
+			)
+		);
+
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'section_content_style',
+			array(
+				'label' => esc_html__( 'Title', 'mas-elementor' ),
 				'tab'   => Controls_Manager::TAB_STYLE,
 			)
 		);
@@ -359,29 +662,36 @@ class Mas_Nav_Tabs extends Base_Widget {
 		$this->end_controls_section();
 
 		$this->start_controls_section(
-			'section_content_style',
+			'section_description_style',
 			array(
-				'label' => esc_html__( 'Content', 'mas-elementor' ),
+				'label' => esc_html__( 'Description', 'mas-elementor' ),
 				'tab'   => Controls_Manager::TAB_STYLE,
 			)
 		);
 
 		$this->add_control(
-			'ul_wrap',
+			'description_color',
 			array(
-				'label'       => esc_html__( 'List Wrap Class', 'mas-elementor' ),
-				'type'        => Controls_Manager::TEXT,
-				'default'     => esc_html__( 'flex-nowrap justify-content-lg-center overflow-auto pb-2 mb-3 mb-lg-4', 'mas-elementor' ),
-				'placeholder' => esc_html__( 'Enter Your Wrap Class', 'mas-elementor' ),
+				'label'     => esc_html__( 'Color', 'mas-elementor' ),
+				'type'      => Controls_Manager::COLOR,
+				'default'   => '',
+				'selectors' => array(
+					'{{WRAPPER}} .elementor-icon-box-description' => 'color: {{VALUE}};',
+				),
+				'global'    => array(
+					'default' => Global_Colors::COLOR_TEXT,
+				),
 			)
 		);
 
-		$this->add_control(
-			'li_wrap',
+		$this->add_group_control(
+			Group_Control_Typography::get_type(),
 			array(
-				'label'       => esc_html__( 'List item Wrap Class', 'mas-elementor' ),
-				'type'        => Controls_Manager::TEXT,
-				'placeholder' => esc_html__( 'Enter Your Wrap Class', 'mas-elementor' ),
+				'name'     => 'description_typography',
+				'selector' => '{{WRAPPER}} .elementor-icon-box-description',
+				'global'   => array(
+					'default' => Global_Typography::TYPOGRAPHY_TEXT,
+				),
 			)
 		);
 
@@ -399,11 +709,14 @@ class Mas_Nav_Tabs extends Base_Widget {
 		$this->add_render_attribute(
 			'list',
 			array(
-				'class' => array( 'mas-nav__tab', 'nav', 'nav-tabs', $settings['ul_wrap'] ),
+				'class' => array( 'mas__nav__tab', 'nav', 'nav-tabs', $settings['ul_wrap'] ),
 				'role'  => 'tablist',
 				'id'    => $list_id,
 			)
 		);
+
+		$this->add_render_attribute( 'description_text', 'class', 'mas__elementor__tab__description' );
+		$this->add_inline_editing_attributes( 'description_text' );
 
 		if ( 'inline' === $settings['view'] ) {
 			$this->add_render_attribute( 'icon_list', 'class', 'elementor-inline-items' );
@@ -453,8 +766,8 @@ class Mas_Nav_Tabs extends Base_Widget {
 				$this->add_render_attribute(
 					'list_link' . $count,
 					array(
-						'class'          => array( 'nav-link text-nowrap', $active ),
-						'id'             => 'si-' . $item['content_id'],
+						'class'          => array( 'nav-link', $active ),
+						'id'             => 'mas-' . $item['content_id'],
 						'data-bs-toggle' => 'tab',
 						'data-bs-target' => '#' . $item['content_id'],
 						'type'           => 'button',
@@ -466,7 +779,7 @@ class Mas_Nav_Tabs extends Base_Widget {
 
 				$icon_class = $item['icon_class'];
 
-				$icon = array( $icon_class, 'si-nav__icon' );
+				$icon = array( $icon_class, 'mas-nav__icon' );
 
 				if ( $item['icon']['value'] ) {
 					$icon[] = $item['icon']['value'];
@@ -482,11 +795,22 @@ class Mas_Nav_Tabs extends Base_Widget {
 				?>
 				<li <?php $this->print_render_attribute_string( 'list_item' . $count ); ?>>
 					<?php if ( isset( $item['list_url']['url'] ) ) : ?>
-						<a  <?php $this->print_render_attribute_string( 'list_link_item' . $count ); ?>><i <?php $this->print_render_attribute_string( 'list_icon' . $count ); ?>></i><?php echo esc_html( $item['list'] ); ?></a>
+						<a  <?php $this->print_render_attribute_string( 'list_link_item' . $count ); ?>>
+							<i <?php $this->print_render_attribute_string( 'list_icon' . $count ); ?>></i>
+							<?php echo esc_html( $item['list'] ); ?>
+							<?php if ( ! empty( $item['description_text'] ) ) : ?>
+								<p <?php $this->print_render_attribute_string( 'description_text' ); ?>><?php echo esc_html( $item['description_text'] ); ?></p>
+							<?php endif; ?>
+						</a>
 						<?php
 					else :
 						?>
-						<button <?php $this->print_render_attribute_string( 'list_link' . $count ); ?>><i <?php $this->print_render_attribute_string( 'list_icon' . $count ); ?>></i><?php echo esc_html( $item['list'] ); ?>
+						<button <?php $this->print_render_attribute_string( 'list_link' . $count ); ?>>
+							<i <?php $this->print_render_attribute_string( 'list_icon' . $count ); ?>></i>
+							<?php echo esc_html( $item['list'] ); ?>
+							<?php if ( ! empty( $item['description_text'] ) ) : ?>
+								<p <?php $this->print_render_attribute_string( 'description_text' ); ?>><?php echo esc_html( $item['description_text'] ); ?></p>
+							<?php endif; ?>
 						</button>
 					<?php endif; ?>
 				</li>
