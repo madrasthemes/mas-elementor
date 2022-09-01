497c47a9df45b175ee1e9fc9109224b8a870db13
Abul Kalam Azad <azad@Transvelo-AbulKalamAzad.local>
2022-09-01 18:28:02 +0530

    Refarction

--- modules/dynamic-tags/tags/job-title.php
+++ modules/dynamic-tags/tags/job-title.php
@@ -11,6 +11,8 @@ use MASElementor\Modules\DynamicTags\Tags\Base\Tag;
 use MASElementor\Modules\DynamicTags\Module;
 use Elementor\Controls_Manager;
 use Elementor\Icons_Manager;
+use Elementor\Core\Kits\Documents\Tabs\Global_Colors;
+
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
@@ -45,11 +47,13 @@ class Job_Title extends \Elementor\Core\DynamicTags\Tag {
 	}
 
 	/**
-	 * Register Controls.
+	 * Register icon widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
 	 */
 	protected function register_controls() {
 		$this->add_control(
-			'job_title_icon',
+			'selected_icon',
 			array(
 				'label'            => esc_html__( 'Icon', 'mas-elementor' ),
 				'type'             => Controls_Manager::ICONS,
@@ -60,24 +64,82 @@ class Job_Title extends \Elementor\Core\DynamicTags\Tag {
 				),
 			)
 		);
+
+		$this->add_control(
+			'primary_color',
+			array(
+				'label'     => esc_html__( 'Primary Color', 'mas-elementor' ),
+				'type'      => Controls_Manager::COLOR,
+				'default'   => '',
+				'selectors' => array(
+					'{{WRAPPER}}.elementor-view-framed .elementor-icon, {{WRAPPER}}.elementor-view-default .elementor-icon' => 'color: {{VALUE}}; border-color: {{VALUE}};',
+					'{{WRAPPER}}.elementor-view-framed .elementor-icon, {{WRAPPER}}.elementor-view-default .elementor-icon svg' => 'fill: {{VALUE}};',
+				),
+				'global'    => array(
+					'default' => Global_Colors::COLOR_PRIMARY,
+				),
+			)
+		);
+
+		$this->add_control(
+			'size',
+			array(
+				'label'     => esc_html__( 'Size', 'mas-elementor' ),
+				'type'      => Controls_Manager::SLIDER,
+				'range'     => array(
+					'px' => array(
+						'min' => 6,
+						'max' => 300,
+					),
+				),
+				'selectors' => array(
+					'{{WRAPPER}} .elementor-icon' => 'font-size: {{SIZE}}{{UNIT}};',
+				),
+				'separator' => 'before',
+			)
+		);
 	}
 
 	/**
-	 * Render the post  title.
+	 * Render icon widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
 	 */
-	public function render() {
+	protected function render() {
 		$settings = $this->get_settings_for_display();
-		$migrated = isset( $settings['__fa4_migrated']['job_title_icon'] );
-		$is_new   = empty( $settings['icon'] ) && Icons_Manager::is_migration_allowed();
-		$featured = is_position_featured();
-		if ( $featured ) {
-			if ( $is_new || $migrated ) :
-				Icons_Manager::render_icon( $settings['job_title_icon'], array( 'aria-hidden' => 'true' ) );
-			else : ?>
-				<i <?php $this->print_render_attribute_string( 'icon' ); ?>></i>
-				<?php
-			endif;
+
+		$this->add_render_attribute( 'wrapper', 'class', 'elementor-icon-wrapper' );
+
+		$this->add_render_attribute( 'icon-wrapper', 'class', 'elementor-icon' );
+
+		$icon_tag = 'div';
+
+		if ( empty( $settings['icon'] ) && ! Icons_Manager::is_migration_allowed() ) {
+			// add old default.
+			$settings['icon'] = 'fa fa-star';
 		}
-		echo wp_kses_post( wpjm_get_the_job_title() );
+
+		if ( ! empty( $settings['icon'] ) ) {
+			$this->add_render_attribute( 'icon', 'class', $settings['icon'] );
+			$this->add_render_attribute( 'icon', 'aria-hidden', 'true' );
+		}
+
+		$migrated = isset( $settings['__fa4_migrated']['selected_icon'] );
+		$is_new   = empty( $settings['icon'] ) && Icons_Manager::is_migration_allowed();
+
+		?>
+		<div <?php $this->print_render_attribute_string( 'wrapper' ); ?>>
+			<<?php echo esc_html( $icon_tag ); ?> <?php $this->print_render_attribute_string( 'icon-wrapper' ); ?>>
+			<?php
+			if ( $is_new || $migrated ) :
+				Icons_Manager::render_icon( $settings['selected_icon'], array( 'aria-hidden' => 'true' ) );
+			else :
+				?>
+				<i <?php $this->print_render_attribute_string( 'icon' ); ?>></i>
+			<?php endif; ?>
+			<<?php echo esc_html( $icon_tag ); ?>>
+		</div>
+		<?php echo wp_kses_post( wpjm_get_the_job_title() ); ?>
+		<?php
 	}
 }
