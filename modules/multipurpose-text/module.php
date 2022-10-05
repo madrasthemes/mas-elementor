8c49aac8845302db8c32b0139c48d89ceac6b30a
YahyaTec <yahya.techy@gmail.com>
2022-10-05 12:42:47 +0530

    #1 added typing text js

--- modules/multipurpose-text/module.php
+++ modules/multipurpose-text/module.php
@@ -19,6 +19,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Module extends Module_Base {
 
 	/**
+	 * Instantiate the class.
+	 */
+	public function __construct() {
+		parent::__construct();
+
+		add_action( 'elementor/frontend/before_register_scripts', array( $this, 'register_frontend_scripts' ) );
+	}
+
+	/**
 	 * Get the widget of the module.
 	 *
 	 * @return string
@@ -36,5 +45,25 @@ class Module extends Module_Base {
 	 */
 	public function get_name() {
 		return 'multipurpose-text';
+	}
+
+	/**
+	 * Register frontend script.
+	 */
+	public function register_frontend_scripts() {
+		wp_register_script(
+			'mas-typed',
+			MAS_ELEMENTOR_MODULES_URL . 'multipurpose-text/assets/js/typed.min.js',
+			array( 'elementor-frontend-modules' ),
+			MAS_ELEMENTOR_VERSION,
+			true
+		);
+		wp_register_script(
+			'mas-typed-init',
+			MAS_ELEMENTOR_MODULES_URL . 'multipurpose-text/assets/js/typed-text.js',
+			array( 'elementor-frontend-modules' ),
+			MAS_ELEMENTOR_VERSION,
+			true
+		);
 	}
 }
