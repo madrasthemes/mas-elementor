f1385084c7d2988b70a130d4083df2b0f910f881
kader-1990 <kader@transvelo.com>
2022-10-19 16:14:17 +0530

    product-related

--- modules/woocommerce/widgets/product-related.php
+++ modules/woocommerce/widgets/product-related.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * The Product Related  Widget.
+ *
+ * @package MASElementor/Modules/Woocommerce/Widgets
+ */
+
 namespace MASElementor\Modules\Woocommerce\Widgets;
 
 use Elementor\Controls_Manager;
@@ -8,25 +14,48 @@ use Elementor\Group_Control_Typography;
 
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit; // Exit if accessed directly.
 }
 
+/**
+ * Class Product Related.
+ */
 class Product_Related extends Products_Base {
 
+	/**
+	 * Get the name of the widget.
+	 *
+	 * @return string
+	 */
 	public function get_name() {
 		return 'mas-woocommerce-product-related';
 	}
 
+	/**
+	 * Get the title of the widget.
+	 *
+	 * @return string
+	 */
 	public function get_title() {
 		return esc_html__( 'Product Related', 'mas-elementor' );
 	}
 
+	/**
+	 * Get the icon of the widget.
+	 *
+	 * @return string
+	 */
 	public function get_icon() {
 		return 'eicon-product-related';
 	}
 
+	/**
+	 * Get the title of the widget.
+	 *
+	 * @return array
+	 */
 	public function get_keywords() {
-		return [ 'woocommerce', 'shop', 'store', 'related', 'similar', 'product' ];
+		return array( 'woocommerce', 'shop', 'store', 'related', 'similar', 'product' );
 	}
 
 	/**
@@ -38,12 +67,17 @@ class Product_Related extends Products_Base {
 		return array( 'mas-elements' );
 	}
 
+	/**
+	 * Register controls for this widget.
+	 *
+	 * @return void
+	 */
 	protected function register_controls() {
 		$this->start_controls_section(
 			'section_related_products_content',
-			[
+			array(
 				'label' => esc_html__( 'Related Products', 'mas-elementor' ),
-			]
+			)
 		);
 
 		$templates = function_exists( 'mas_template_options' ) ? mas_template_options() : array();
@@ -58,56 +92,60 @@ class Product_Related extends Products_Base {
 
 		$this->add_control(
 			'posts_per_page',
-			[
-				'label' => esc_html__( 'Products Per Page', 'mas-elementor' ),
-				'type' => Controls_Manager::NUMBER,
+			array(
+				'label'   => esc_html__( 'Products Per Page', 'mas-elementor' ),
+				'type'    => Controls_Manager::NUMBER,
 				'default' => 4,
-				'range' => [
-					'px' => [
+				'range'   => array(
+					'px' => array(
 						'max' => 20,
-					],
-				],
-			]
+					),
+				),
+			)
 		);
 
 		$this->add_columns_responsive_control();
 
 		$this->add_control(
 			'orderby',
-			[
-				'label' => esc_html__( 'Order By', 'mas-elementor' ),
-				'type' => Controls_Manager::SELECT,
+			array(
+				'label'   => esc_html__( 'Order By', 'mas-elementor' ),
+				'type'    => Controls_Manager::SELECT,
 				'default' => 'date',
-				'options' => [
-					'date' => esc_html__( 'Date', 'mas-elementor' ),
-					'title' => esc_html__( 'Title', 'mas-elementor' ),
-					'price' => esc_html__( 'Price', 'mas-elementor' ),
+				'options' => array(
+					'date'       => esc_html__( 'Date', 'mas-elementor' ),
+					'title'      => esc_html__( 'Title', 'mas-elementor' ),
+					'price'      => esc_html__( 'Price', 'mas-elementor' ),
 					'popularity' => esc_html__( 'Popularity', 'mas-elementor' ),
-					'rating' => esc_html__( 'Rating', 'mas-elementor' ),
-					'rand' => esc_html__( 'Random', 'mas-elementor' ),
+					'rating'     => esc_html__( 'Rating', 'mas-elementor' ),
+					'rand'       => esc_html__( 'Random', 'mas-elementor' ),
 					'menu_order' => esc_html__( 'Menu Order', 'mas-elementor' ),
-				],
-			]
+				),
+			)
 		);
 
 		$this->add_control(
 			'order',
-			[
-				'label' => esc_html__( 'Order', 'mas-elementor' ),
-				'type' => Controls_Manager::SELECT,
+			array(
+				'label'   => esc_html__( 'Order', 'mas-elementor' ),
+				'type'    => Controls_Manager::SELECT,
 				'default' => 'desc',
-				'options' => [
-					'asc' => esc_html__( 'ASC', 'mas-elementor' ),
+				'options' => array(
+					'asc'  => esc_html__( 'ASC', 'mas-elementor' ),
 					'desc' => esc_html__( 'DESC', 'mas-elementor' ),
-				],
-			]
+				),
+			)
 		);
 
 		$this->end_controls_section();
 
-		
 	}
 
+	/**
+	 * Render.
+	 *
+	 * @return void
+	 */
 	protected function render() {
 		global $product;
 
@@ -119,12 +157,12 @@ class Product_Related extends Products_Base {
 
 		$settings = $this->get_settings_for_display();
 
-		$args = [
+		$args = array(
 			'posts_per_page' => 4,
-			'columns' => 4,
-			'orderby' => $settings['orderby'],
-			'order' => $settings['order'],
-		];
+			'columns'        => 4,
+			'orderby'        => $settings['orderby'],
+			'order'          => $settings['order'],
+		);
 
 		if ( ! empty( $settings['posts_per_page'] ) ) {
 			$args['posts_per_page'] = $settings['posts_per_page'];
@@ -140,19 +178,29 @@ class Product_Related extends Products_Base {
 		// Handle orderby.
 		$args['related_products'] = wc_products_array_orderby( $args['related_products'], $args['orderby'], $args['order'] );
 
-		
-
-		mas_elementor_get_template( 'widgets/single-products/related.php', array( 'related_products' => $args['related_products'], 'widget' => $this ) );
-
-		
+		mas_elementor_get_template(
+			'widgets/single-products/related.php',
+			array(
+				'related_products' => $args['related_products'],
+				'widget'           => $this,
+			)
+		);
 
 	}
 
+	/**
+	 * Render Plain Content
+	 */
 	public function render_plain_content() {}
 
+	/**
+	 * Get the group name of the widget.
+	 *
+	 * @return string
+	 */
 	public function get_group_name() {
 		return 'woocommerce';
 	}
 
-		
+
 }
