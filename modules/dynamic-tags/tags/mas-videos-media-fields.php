3f221fc8304e85aa40ffa533be31fafd0f72ccab
YahyaTec <yahya.techy@gmail.com>
2022-10-28 15:21:06 +0530

    #28 added tv shows cast and crew featured image

--- modules/dynamic-tags/tags/mas-videos-media-fields.php
+++ modules/dynamic-tags/tags/mas-videos-media-fields.php
@@ -48,24 +48,74 @@ class Mas_Videos_Media_Fields extends \Elementor\Core\DynamicTags\Data_Tag {
 	 * @param array $options control opions.
 	 */
 	public function get_value( array $options = array() ) {
-		$movie   = masvideos_get_movie( get_the_ID() );
+		$movie = masvideos_get_movie( get_the_ID() );
 		$tv_show = masvideos_get_tv_show( get_the_ID() );
-		$video   = masvideos_get_video( get_the_ID() );
+		$video = masvideos_get_video( get_the_ID() );
 
 		$settings   = $this->get_settings();
 		$image_data = $this->get_settings( 'fallback' );
 		if ( ! empty( $movie ) && 'person' === $settings['mas_videos_media_url'] ) {
-			$crews = $movie->get_crew();
-			if ( ! empty( $crews ) ) {
-				foreach ( $crews as $key => $crew ) {
-					if ( $key === (int) $settings['mas_movie_person_ids'] ) {
-						$person     = masvideos_get_person( $crew['id'] );
-						$media_id   = $person->get_image_id();
-						$media_url  = wp_get_attachment_url( $media_id );
-						$image_data = array(
-							'id'  => $person->get_image_id(),
-							'url' => $media_url,
-						);
+			if ( 'crew' === $settings['mas_videos_casts_crew'] ) {
+				$crews = $movie->get_crew();
+				if ( ! empty( $crews ) ) {
+					foreach ( $crews as $key => $crew ) {
+						if ( $key === (int) $settings['mas_movie_person_ids'] ) {
+							$person     = masvideos_get_person( $crew['id'] );
+							$media_id   = $person->get_image_id();
+							$media_url  = wp_get_attachment_url( $media_id );
+							$image_data = array(
+								'id'  => $person->get_image_id(),
+								'url' => $media_url,
+							);
+						}
+					}
+				}
+			} else {
+				$casts = $movie->get_cast();
+				if ( ! empty( $casts ) ) {
+					foreach ( $casts as $key => $cast ) {
+						if ( $key === (int) $settings['mas_movie_person_ids'] ) {
+							$person     = masvideos_get_person( $cast['id'] );
+							$media_id   = $person->get_image_id();
+							$media_url  = wp_get_attachment_url( $media_id );
+							$image_data = array(
+								'id'  => $person->get_image_id(),
+								'url' => $media_url,
+							);
+						}
+					}
+				}
+			}
+		}
+		if ( ! empty( $tv_show ) && 'tv_person' === $settings['mas_videos_media_url'] ) {
+			if ( 'crew' === $settings['mas_videos_casts_crew'] ) {
+				$crews = $tv_show->get_crew();
+				if ( ! empty( $crews ) ) {
+					foreach ( $crews as $key => $crew ) {
+						if ( $key === (int) $settings['mas_movie_person_ids'] ) {
+							$person     = masvideos_get_person( $crew['id'] );
+							$media_id   = $person->get_image_id();
+							$media_url  = wp_get_attachment_url( $media_id );
+							$image_data = array(
+								'id'  => $person->get_image_id(),
+								'url' => $media_url,
+							);
+						}
+					}
+				}
+			} else {
+				$casts = $tv_show->get_cast();
+				if ( ! empty( $casts ) ) {
+					foreach ( $casts as $key => $cast ) {
+						if ( $key === (int) $settings['mas_movie_person_ids'] ) {
+							$person     = masvideos_get_person( $cast['id'] );
+							$media_id   = $person->get_image_id();
+							$media_url  = wp_get_attachment_url( $media_id );
+							$image_data = array(
+								'id'  => $person->get_image_id(),
+								'url' => $media_url,
+							);
+						}
 					}
 				}
 			}
@@ -98,14 +148,51 @@ class Mas_Videos_Media_Fields extends \Elementor\Core\DynamicTags\Data_Tag {
 		$this->add_control(
 			'mas_videos_media_url',
 			array(
-				'label'   => esc_html__( 'URL', 'mas-elementor' ),
+				'label'   => esc_html__( 'Type', 'mas-elementor' ),
 				'type'    => Controls_Manager::SELECT,
 				'default' => 'movie',
 				'options' => array(
-					'movie'   => esc_html__( 'Movies', 'mas-elementor' ),
-					'tv_show' => esc_html__( 'TV Show', 'mas-elementor' ),
-					'video'   => esc_html__( 'Videos', 'mas-elementor' ),
-					'person'  => esc_html__( 'Movie Persons', 'mas-elementor' ),
+					'movie'      => esc_html__( 'Movies', 'mas-elementor' ),
+					'tv_show'    => esc_html__( 'TV Show', 'mas-elementor' ),
+					'video'      => esc_html__( 'Videos', 'mas-elementor' ),
+					'person'     => esc_html__( 'Movie Casts and Crew', 'mas-elementor' ),
+					'tv_person'  => esc_html__( 'TV Show Casts and Crew', 'mas-elementor' ),
+				),
+			)
+		);
+
+		$this->add_control(
+			'mas_videos_casts_crew',
+			array(
+				'label'   => esc_html__( 'Casts or Crew', 'mas-elementor' ),
+				'type'    => Controls_Manager::SELECT,
+				'default' => 'cast',
+				'options' => array(
+					'cast' => esc_html__( 'Casts', 'mas-elementor' ),
+					'crew' => esc_html__( 'Crew', 'mas-elementor' ),
+				),
+				'conditions' => array(
+					'relation' => 'or',
+					'terms'    => array(
+						array(
+							'terms' => array(
+								array(
+									'name'     => 'mas_videos_media_url',
+									'operator' => '===',
+									'value'    => 'person',
+								),
+							),
+						),
+						array(
+							'terms' => array(
+								array(
+									'name'     => 'mas_videos_media_url',
+									'operator' => '===',
+									'value'    => 'tv_person',
+								),
+							),
+						),
+					),
 				),
 			)
 		);
@@ -124,8 +211,28 @@ class Mas_Videos_Media_Fields extends \Elementor\Core\DynamicTags\Data_Tag {
 				'default'     => '0',
 				'description' => esc_html__( 'Person here shown are in movie crew options and in ordered', 'mas-elementor' ),
 				'options'     => $crew_options,
-				'condition'   => array(
-					'mas_videos_media_url' => 'person',
+				'conditions' => array(
+					'relation' => 'or',
+					'terms'    => array(
+						array(
+							'terms' => array(
+								array(
+									'name'     => 'mas_videos_media_url',
+									'operator' => '===',
+									'value'    => 'person',
+								),
+							),
+						),
+						array(
+							'terms' => array(
+								array(
+									'name'     => 'mas_videos_media_url',
+									'operator' => '===',
+									'value'    => 'tv_person',
+								),
+							),
+						),
+					),
 				),
 			)
 		);
