f34adc895eb8adf62067b583ed164645def9f043
YahyaTec <yahya.techy@gmail.com>
2022-10-28 10:41:35 +0530

    #28 added tv show cast and crew options

--- modules/dynamic-tags/tags/mas-videos-text-fields.php
+++ modules/dynamic-tags/tags/mas-videos-text-fields.php
@@ -70,10 +70,90 @@ class Mas_Videos_Text_Fields extends \Elementor\Core\DynamicTags\Tag {
 					'censor_rating' => esc_html__( 'Censor Rating', 'mas-elementor' ),
 					'imdb_id'       => esc_html__( 'IMDB ID', 'mas-elementor' ),
 					'tmdb_id'       => esc_html__( 'TMDB ID', 'mas-elementor' ),
-					'crew'          => esc_html__( 'Crew', 'mas-elementor' ),
+					'casts_crew'    => esc_html__( 'Casts and Crew', 'mas-elementor' ),
 				),
 				'condition' => array(
 					'mas_videos_post_types' => 'movie',
+				),
+			)
+		);
+
+		$this->add_control(
+			'mas_tv_show_text_options',
+			array(
+				'label'     => esc_html__( 'Type', 'mas-elementor' ),
+				'type'      => Controls_Manager::SELECT,
+				'default'   => 'imdb_id',
+				'options'   => array(
+					'imdb_id'           => esc_html__( 'IMDB ID', 'mas-elementor' ),
+					'tmdb_id'           => esc_html__( 'TMDB ID', 'mas-elementor' ),
+					'season_names'      => esc_html__( 'Season Names', 'mas-elementor' ),
+					'season_start_year' => esc_html__( 'Seasons Start Year', 'mas-elementor' ),
+					'season_end_year'   => esc_html__( 'Seasons End Year', 'mas-elementor' ),
+					'casts_crew'        => esc_html__( 'Casts and Crew', 'mas-elementor' ),
+				),
+				'condition' => array(
+					'mas_videos_post_types' => 'tv_show',
+				),
+			)
+		);
+
+		$this->add_control(
+			'season_names_separator',
+			array(
+				'label'     => esc_html__( 'Separator', 'mas-elementor' ),
+				'type'      => Controls_Manager::TEXT,
+				'default'   => ', ',
+				'condition' => array(
+					'mas_videos_post_types'    => 'tv_show',
+					'mas_tv_show_text_options' => 'season_names',
+				),
+
+			)
+		);
+
+		$this->add_control(
+			'mas_videos_person_options',
+			array(
+				'label'      => esc_html__( 'Persons', 'mas-elementor' ),
+				'type'       => Controls_Manager::SELECT,
+				'default'    => 'crew',
+				'options'    => array(
+					'casts' => esc_html__( 'Casts', 'mas-elementor' ),
+					'crew'  => esc_html__( 'Crew', 'mas-elementor' ),
+				),
+				'conditions' => array(
+					'relation' => 'or',
+					'terms'    => array(
+						array(
+							'terms' => array(
+								array(
+									'name'     => 'mas_videos_post_types',
+									'operator' => '===',
+									'value'    => 'movie',
+								),
+								array(
+									'name'     => 'mas_movie_text_options',
+									'operator' => '===',
+									'value'    => 'casts_crew',
+								),
+							),
+						),
+						array(
+							'terms' => array(
+								array(
+									'name'     => 'mas_videos_post_types',
+									'operator' => '===',
+									'value'    => 'tv_show',
+								),
+								array(
+									'name'     => 'mas_tv_show_text_options',
+									'operator' => '===',
+									'value'    => 'casts_crew',
+								),
+							),
+						),
+					),
 				),
 			)
 		);
@@ -90,27 +170,153 @@ class Mas_Videos_Text_Fields extends \Elementor\Core\DynamicTags\Tag {
 				'label'       => esc_html__( 'Select Person', 'mas-elementor' ),
 				'type'        => Controls_Manager::SELECT,
 				'description' => esc_html__( 'Person here shown are in movie crew options and in ordered', 'mas-elementor' ),
+				'default'     => '0',
 				'options'     => $crew_options,
-				'condition'   => array(
-					'mas_videos_post_types'  => 'movie',
-					'mas_movie_text_options' => 'crew',
+				'conditions'  => array(
+					'relation' => 'or',
+					'terms'    => array(
+						array(
+							'terms' => array(
+								array(
+									'name'     => 'mas_videos_post_types',
+									'operator' => '===',
+									'value'    => 'movie',
+								),
+								array(
+									'name'     => 'mas_movie_text_options',
+									'operator' => '===',
+									'value'    => 'casts_crew',
+								),
+							),
+						),
+						array(
+							'terms' => array(
+								array(
+									'name'     => 'mas_videos_post_types',
+									'operator' => '===',
+									'value'    => 'tv_show',
+								),
+								array(
+									'name'     => 'mas_tv_show_text_options',
+									'operator' => '===',
+									'value'    => 'casts_crew',
+								),
+							),
+						),
+					),
+				),
+
+			)
+		);
+
+		$this->add_control(
+			'mas_movie_person_crew_options',
+			array(
+				'label'      => esc_html__( 'Person options', 'mas-elementor' ),
+				'type'       => Controls_Manager::SELECT,
+				'default'    => 'name',
+				'options'    => array(
+					'name' => esc_html__( 'Name', 'mas-elementor' ),
+					'job'  => esc_html__( 'Job', 'mas-elementor' ),
+				),
+				'conditions' => array(
+					'relation' => 'or',
+					'terms'    => array(
+						array(
+							'terms' => array(
+								array(
+									'name'     => 'mas_videos_post_types',
+									'operator' => '===',
+									'value'    => 'movie',
+								),
+								array(
+									'name'     => 'mas_videos_person_options',
+									'operator' => '===',
+									'value'    => 'crew',
+								),
+								array(
+									'name'     => 'mas_movie_text_options',
+									'operator' => '===',
+									'value'    => 'casts_crew',
+								),
+							),
+						),
+						array(
+							'terms' => array(
+								array(
+									'name'     => 'mas_videos_post_types',
+									'operator' => '===',
+									'value'    => 'tv_show',
+								),
+								array(
+									'name'     => 'mas_videos_person_options',
+									'operator' => '===',
+									'value'    => 'crew',
+								),
+								array(
+									'name'     => 'mas_tv_show_text_options',
+									'operator' => '===',
+									'value'    => 'casts_crew',
+								),
+							),
+						),
+					),
 				),
 			)
 		);
 
 		$this->add_control(
-			'mas_movie_person_options',
+			'mas_movie_person_cast_options',
 			array(
-				'label'     => esc_html__( 'Person options', 'mas-elementor' ),
-				'type'      => Controls_Manager::SELECT,
-				'default'   => 'name',
-				'options'   => array(
-					'name' => esc_html__( 'Name', 'mas-elementor' ),
-					'job'  => esc_html__( 'Job', 'mas-elementor' ),
+				'label'      => esc_html__( 'Person options', 'mas-elementor' ),
+				'type'       => Controls_Manager::SELECT,
+				'default'    => 'name',
+				'options'    => array(
+					'name'      => esc_html__( 'Name', 'mas-elementor' ),
+					'character' => esc_html__( 'Character', 'mas-elementor' ),
 				),
-				'condition' => array(
-					'mas_videos_post_types'  => 'movie',
-					'mas_movie_text_options' => 'crew',
+				'conditions' => array(
+					'relation' => 'or',
+					'terms'    => array(
+						array(
+							'terms' => array(
+								array(
+									'name'     => 'mas_videos_post_types',
+									'operator' => '===',
+									'value'    => 'movie',
+								),
+								array(
+									'name'     => 'mas_videos_person_options',
+									'operator' => '===',
+									'value'    => 'casts',
+								),
+								array(
+									'name'     => 'mas_movie_text_options',
+									'operator' => '===',
+									'value'    => 'casts_crew',
+								),
+							),
+						),
+						array(
+							'terms' => array(
+								array(
+									'name'     => 'mas_videos_post_types',
+									'operator' => '===',
+									'value'    => 'tv_show',
+								),
+								array(
+									'name'     => 'mas_videos_person_options',
+									'operator' => '===',
+									'value'    => 'casts',
+								),
+								array(
+									'name'     => 'mas_tv_show_text_options',
+									'operator' => '===',
+									'value'    => 'casts_crew',
+								),
+							),
+						),
+					),
 				),
 			)
 		);
@@ -148,44 +354,12 @@ class Mas_Videos_Text_Fields extends \Elementor\Core\DynamicTags\Tag {
 			)
 		);
 
-		$this->add_control(
-			'mas_tv_show_text_options',
-			array(
-				'label'     => esc_html__( 'Type', 'mas-elementor' ),
-				'type'      => Controls_Manager::SELECT,
-				'default'   => 'imdb_id',
-				'options'   => array(
-					'imdb_id'           => esc_html__( 'IMDB ID', 'mas-elementor' ),
-					'tmdb_id'           => esc_html__( 'TMDB ID', 'mas-elementor' ),
-					'season_names'      => esc_html__( 'Season Names', 'mas-elementor' ),
-					'season_start_year' => esc_html__( 'Seasons Start Year', 'mas-elementor' ),
-					'season_end_year'   => esc_html__( 'Seasons End Year', 'mas-elementor' ),
-				),
-				'condition' => array(
-					'mas_videos_post_types' => 'tv_show',
-				),
-			)
-		);
-
-		$this->add_control(
-			'season_names_separator',
-			array(
-				'label'     => esc_html__( 'Separator', 'mas-elementor' ),
-				'type'      => Controls_Manager::TEXT,
-				'default'   => ', ',
-				'condition' => array(
-					'mas_videos_post_types'    => 'tv_show',
-					'mas_tv_show_text_options' => 'season_names',
-				),
-
-			)
-		);
 	}
 	/**
 	 * Render the post  title.
 	 */
 	public function render() {
-		$movie = masvideos_get_movie( get_the_ID() );
+		$movie   = masvideos_get_movie( get_the_ID() );
 		$tv_show = masvideos_get_tv_show( get_the_ID() );
 
 		$settings    = $this->get_settings();
@@ -209,15 +383,28 @@ class Mas_Videos_Text_Fields extends \Elementor\Core\DynamicTags\Tag {
 			if ( 'tmdb_id' === $settings['mas_movie_text_options'] ) {
 				$text_output = $movie->get_tmdb_id();
 			}
-			if ( 'crew' === $settings['mas_movie_text_options'] ) {
+			if ( 'crew' === $settings['mas_videos_person_options'] && 'casts_crew' === $settings['mas_movie_text_options'] ) {
 				$crews = $movie->get_crew();
 				if ( ! empty( $crews ) ) {
 					foreach ( $crews as $key => $crew ) {
-						if ( $key === (int) $settings['mas_movie_person_ids'] && 'name' === $settings['mas_movie_person_options'] ) {
+						if ( $key === (int) $settings['mas_movie_person_ids'] && 'name' === $settings['mas_movie_person_crew_options'] ) {
 							$text_output = get_the_title( $crew['id'] );
 						}
-						if ( $key === (int) $settings['mas_movie_person_ids'] && 'job' === $settings['mas_movie_person_options'] ) {
+						if ( $key === (int) $settings['mas_movie_person_ids'] && 'job' === $settings['mas_movie_person_crew_options'] ) {
 							$text_output = $crew['job'];
+						}
+					}
+				}
+			}
+			if ( 'casts' === $settings['mas_videos_person_options'] && 'casts_crew' === $settings['mas_movie_text_options'] ) {
+				$casts = $movie->get_cast();
+				if ( ! empty( $casts ) ) {
+					foreach ( $casts as $key => $cast ) {
+						if ( $key === (int) $settings['mas_movie_person_ids'] && 'name' === $settings['mas_movie_person_cast_options'] ) {
+							$text_output = get_the_title( $cast['id'] );
+						}
+						if ( $key === (int) $settings['mas_movie_person_ids'] && 'character' === $settings['mas_movie_person_cast_options'] ) {
+							$text_output = $cast['character'];
 						}
 					}
 				}
@@ -259,6 +446,32 @@ class Mas_Videos_Text_Fields extends \Elementor\Core\DynamicTags\Tag {
 				} elseif ( 'season_names' === $settings['mas_tv_show_text_options'] ) {
 					$season_names = implode( $settings['season_names_separator'], $season_names );
 					$text_output  = $season_names;
+				}
+			}
+			if ( 'crew' === $settings['mas_videos_person_options'] && 'casts_crew' === $settings['mas_tv_show_text_options'] ) {
+				$crews = $tv_show->get_crew();
+				if ( ! empty( $crews ) ) {
+					foreach ( $crews as $key => $crew ) {
+						if ( $key === (int) $settings['mas_movie_person_ids'] && 'name' === $settings['mas_movie_person_crew_options'] ) {
+							$text_output = get_the_title( $crew['id'] );
+						}
+						if ( $key === (int) $settings['mas_movie_person_ids'] && 'job' === $settings['mas_movie_person_crew_options'] ) {
+							$text_output = $crew['job'];
+						}
+					}
+				}
+			}
+			if ( 'casts' === $settings['mas_videos_person_options'] && 'casts_crew' === $settings['mas_tv_show_text_options'] ) {
+				$casts = $tv_show->get_cast();
+				if ( ! empty( $casts ) ) {
+					foreach ( $casts as $key => $cast ) {
+						if ( $key === (int) $settings['mas_movie_person_ids'] && 'name' === $settings['mas_movie_person_cast_options'] ) {
+							$text_output = get_the_title( $cast['id'] );
+						}
+						if ( $key === (int) $settings['mas_movie_person_ids'] && 'character' === $settings['mas_movie_person_cast_options'] ) {
+							$text_output = $cast['character'];
+						}
+					}
 				}
 			}
 		}
