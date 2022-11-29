c2cd744114f3492486c8017e2c0c70168d15c2f2
malicksm <malick@transvelo.com>
2022-11-29 13:24:51 +0530

    #38

--- modules/jobs-filter/widgets/jobs-filter.php
+++ modules/jobs-filter/widgets/jobs-filter.php
@@ -1236,6 +1236,7 @@ class Jobs_Filter extends Base_Widget {
 				],
 				'condition'   => array(
 					'search_icon[value]!' => '',
+					'search_text!' => '',
 				),
 			]
 		);
@@ -1433,16 +1434,76 @@ class Jobs_Filter extends Base_Widget {
 		);
 	}
 
-    /**
-     * Register Skins.
+	 /**
+     * Render.
      */
-    protected function register_skins() {
+    public function render(){
+        $jobs_page_url = get_page_link( get_option( 'job_manager_jobs_page_id' ) );
+        $settings  = $this->get_settings_for_display();
 
-            $this->add_skin( new Skins\Skin_Style_1( $this ) );
-            $this->add_skin( new Skins\Skin_Style_2( $this ) );
-            $this->add_skin( new Skins\Skin_Style_3( $this ) );
-            $this->add_skin( new Skins\Skin_Style_4( $this ) );
-            $this->add_skin( new Skins\Skin_Style_5( $this ) );
+        $this->add_render_attribute( 
+			'icon-align',
+			'class', 
+            [
+                'mas-job-search-icon',
+                'mas-job-search-icon-' . $settings['search_icon_align'],
+                $settings['search_icon']['value']
+            ]
+		);
+
+        ?>
+        <form method="GET" action="<?php echo esc_url( $jobs_page_url ); ?>" class="mas-search-form" style="display:flex;">
+            <div class="mas-job-search-keywords">
+                <label class="sr-only" for="search_keywords"><?php echo esc_html( 'keyword' ); ?></label>
+                <input type="text" id="search_keywords" name="search_keywords" placeholder="<?php echo esc_html( $settings['keyword_placeholder'] ); ?>" class="mas-elementor-search-keywords ui-autocomplete-input" autocomplete="off">
+                <?php if( ! empty( $settings['job_title_icon']['value'] ) ):
+                    ?><i class="<?php echo esc_attr( $settings['job_title_icon']['value'] ); ?>"></i>
+                <?php endif;?>
+            </div>
+            <?php
+            if( 'yes' === $settings['show_category'] && '2' === $settings['category_position'] ):?>
+                <div class="mas-job-search-category">
+                    <label class="sr-only" for="search_category"><?php echo esc_html__( 'Category', 'mas-elementor' ); ?></label>
+                    <select id="search_category" name="search_category">
+                        <option value=""><?php echo esc_html( $settings['category_placeholder'] ); ?></option>
+                        <?php foreach ( get_job_listing_categories() as $cat ) : ?>
+                        <option value="<?php echo esc_attr( $cat->term_id ); ?>"><?php echo esc_html( $cat->name ); ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+            <?php endif;?>
+            <div class="mas-job-search-location">
+                <label class="sr-only" for="search_location"><?php echo esc_html( 'location'); ?></label>
+                <input type="text" id="search_location" name="search_location" placeholder="<?php echo esc_html( $settings['location_placeholder'] ); ?>" class="mas-elementor-search-location pac-target-input">
+                <?php if( ! empty( $settings['location_icon']['value'] ) ):
+                    ?><i class="<?php echo esc_attr( $settings['location_icon']['value'] ); ?>"></i>
+                <?php endif;?>
+            </div>
+            <?php
+            if( 'yes' === $settings['show_category'] && '3' === $settings['category_position'] ):?>
+                <div class="mas-job-search-category">
+                    <label class="sr-only" for="search_category"><?php echo esc_html__( 'Category', 'mas-elementor' ); ?></label>
+                    <select id="search_category" name="search_category">
+                        <option value=""><?php echo esc_html( $settings['category_placeholder'] ); ?></option>
+                        <?php foreach ( get_job_listing_categories() as $cat ) : ?>
+                        <option value="<?php echo esc_attr( $cat->term_id ); ?>"><?php echo esc_html( $cat->name ); ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+            <?php endif;?>
+            <div class="mas-job-search-submit job-search-submit">
+                <button type="submit" value="Search">
+                <?php if( ! empty( $settings['search_icon']['value'] ) ):
+                    ?>
+                    <i <?php $this->print_render_attribute_string( 'icon-align' ); ?>></i>
+                <?php endif;?>
+                <?php if( ! empty( $settings['search_text'] ) ):
+                    ?><span class="mas-job-search-text"><?php echo esc_html( $settings['search_text'] ); ?></span>
+                <?php endif;?>
+                </button>
+            </div>
+            <input type="hidden" name="post_type" value="job_listing">
+        </form><?php
     }
 
 }
