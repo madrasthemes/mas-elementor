778eb131a53031b9d47be58569e5cbbc64717a5a
YahyaTec <yahya.techy@gmail.com>
2023-06-28 10:38:36 +0530

    post content fix

--- modules/post-content/widgets/post-content.php
+++ modules/post-content/widgets/post-content.php
@@ -155,7 +155,7 @@ class Post_Content extends Base_Widget {
 			)
 		);
 
-		$exclude_posttype = apply_filters( 'mas_post_content_exclude_posttype', array() );
+		$exclude_posttype = apply_filters( 'mas_post_content_exclude_posttype', array('page') );
 
 		if ( is_array( $allowed_posttypes ) ) {
 			foreach ( $allowed_posttypes as $allowed_posttype ) {
