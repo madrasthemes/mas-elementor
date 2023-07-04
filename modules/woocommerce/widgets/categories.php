bce894ca68de0544e43eb9de13b80e51573cfcf2
YahyaTec <yahya.techy@gmail.com>
2023-07-04 17:33:34 +0530

    added product categories

---
+++ modules/woocommerce/widgets/categories.php
@@ -0,0 +1,463 @@
+<?php
+/**
+ * The Categories Widget.
+ *
+ * @package MASElementor/Modules/Woocommerce/Widgets
+ */
+
+namespace MASElementor\Modules\Woocommerce\Widgets;
+
+use Elementor\Controls_Manager;
+use Elementor\Core\Kits\Documents\Tabs\Global_Colors;
+use Elementor\Core\Kits\Documents\Tabs\Global_Typography;
+use Elementor\Group_Control_Border;
+use Elementor\Group_Control_Typography;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Class Categories
+ */
+class Categories extends Base_Widget {
+
+	/**
+	 * Has content template
+	 *
+	 * @var _has_template_content
+	 */
+	protected $_has_template_content = false; // phpcs:ignore PSR2.Classes.PropertyDeclaration.Underscore
+
+	/**
+	 * Get the name of the widget.
+	 *
+	 * @return string
+	 */
+	public function get_name() {
+		return 'mas-product-categories';
+	}
+
+	/**
+	 * Get the title of the widget.
+	 *
+	 * @return string
+	 */
+	public function get_title() {
+		return esc_html__( 'Product Categories', 'mas-elementor' );
+	}
+
+	/**
+	 * Get the icon of the widget.
+	 *
+	 * @return string
+	 */
+	public function get_icon() {
+		return 'eicon-product-categories';
+	}
+
+	/**
+	 * Get the keywords related to the widget.
+	 *
+	 * @return array
+	 */
+	public function get_keywords() {
+		return array( 'woocommerce-elements', 'shop', 'store', 'categories', 'product' );
+	}
+
+	/**
+	 * Get the categories for the widget.
+	 *
+	 * @return array
+	 */
+	public function get_categories() {
+		return array(
+			'woocommerce-elements',
+		);
+	}
+
+	/**
+	 * Register controls for this widget.
+	 */
+	protected function register_controls() {
+		$this->start_controls_section(
+			'section_layout',
+			array(
+				'label' => esc_html__( 'Layout', 'mas-elementor' ),
+				'tab'   => Controls_Manager::TAB_CONTENT,
+			)
+		);
+
+		$this->add_columns_responsive_control();
+
+		$this->add_control(
+			'number',
+			array(
+				'label'   => esc_html__( 'Categories Count', 'mas-elementor' ),
+				'type'    => Controls_Manager::NUMBER,
+				'default' => '4',
+			)
+		);
+
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'section_filter',
+			array(
+				'label' => esc_html__( 'Query', 'mas-elementor' ),
+				'tab'   => Controls_Manager::TAB_CONTENT,
+			)
+		);
+
+		$this->add_control(
+			'source',
+			array(
+				'label'       => esc_html__( 'Source', 'mas-elementor' ),
+				'type'        => Controls_Manager::SELECT,
+				'options'     => array(
+					''                      => esc_html__( 'Show All', 'mas-elementor' ),
+					'by_id'                 => esc_html__( 'Manual Selection', 'mas-elementor' ),
+					'by_parent'             => esc_html__( 'By Parent', 'mas-elementor' ),
+					'current_subcategories' => esc_html__( 'Current Subcategories', 'mas-elementor' ),
+				),
+				'label_block' => true,
+			)
+		);
+
+		$categories = get_terms( 'product_cat' );
+
+		$options = array();
+		foreach ( $categories as $category ) {
+			$options[ $category->term_id ] = $category->name;
+		}
+
+		$this->add_control(
+			'categories',
+			array(
+				'label'       => esc_html__( 'Categories', 'mas-elementor' ),
+				'type'        => Controls_Manager::SELECT2,
+				'options'     => $options,
+				'default'     => array(),
+				'label_block' => true,
+				'multiple'    => true,
+				'condition'   => array(
+					'source' => 'by_id',
+				),
+			)
+		);
+
+		$parent_options = array( '0' => esc_html__( 'Only Top Level', 'mas-elementor' ) ) + $options;
+		$this->add_control(
+			'parent',
+			array(
+				'label'     => esc_html__( 'Parent', 'mas-elementor' ),
+				'type'      => Controls_Manager::SELECT,
+				'default'   => '0',
+				'options'   => $parent_options,
+				'condition' => array(
+					'source' => 'by_parent',
+				),
+			)
+		);
+
+		$this->add_control(
+			'hide_empty',
+			array(
+				'label'     => esc_html__( 'Hide Empty', 'mas-elementor' ),
+				'type'      => Controls_Manager::SWITCHER,
+				'default'   => '',
+				'label_on'  => 'Hide',
+				'label_off' => 'Show',
+			)
+		);
+
+		$this->add_control(
+			'orderby',
+			array(
+				'label'   => esc_html__( 'Order By', 'mas-elementor' ),
+				'type'    => Controls_Manager::SELECT,
+				'default' => 'name',
+				'options' => array(
+					'name'        => esc_html__( 'Name', 'mas-elementor' ),
+					'slug'        => esc_html__( 'Slug', 'mas-elementor' ),
+					'description' => esc_html__( 'Description', 'mas-elementor' ),
+					'count'       => esc_html__( 'Count', 'mas-elementor' ),
+				),
+			)
+		);
+
+		$this->add_control(
+			'order',
+			array(
+				'label'   => esc_html__( 'Order', 'mas-elementor' ),
+				'type'    => Controls_Manager::SELECT,
+				'default' => 'desc',
+				'options' => array(
+					'asc'  => esc_html__( 'ASC', 'mas-elementor' ),
+					'desc' => esc_html__( 'DESC', 'mas-elementor' ),
+				),
+			)
+		);
+
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'section_products_style',
+			array(
+				'label' => esc_html__( 'Products', 'mas-elementor' ),
+				'tab'   => Controls_Manager::TAB_STYLE,
+			)
+		);
+
+		$this->add_control(
+			'wc_style_warning',
+			array(
+				'type'            => Controls_Manager::RAW_HTML,
+				'raw'             => esc_html__( 'The style of this widget is often affected by your theme and plugins. If you experience any such issue, try to switch to a basic theme and deactivate related plugins.', 'mas-elementor' ),
+				'content_classes' => 'elementor-panel-alert elementor-panel-alert-info',
+			)
+		);
+
+		$this->add_control(
+			'products_class',
+			array(
+				'type'         => Controls_Manager::HIDDEN,
+				'default'      => 'wc-products',
+				'prefix_class' => 'elementor-products-grid elementor-',
+			)
+		);
+
+		$this->add_control(
+			'column_gap',
+			array(
+				'label'      => esc_html__( 'Columns Gap', 'mas-elementor' ),
+				'type'       => Controls_Manager::SLIDER,
+				'size_units' => array( 'px', 'em', 'rem', 'custom' ),
+				'default'    => array(
+					'size' => 20,
+				),
+				'range'      => array(
+					'px' => array(
+						'min' => 0,
+						'max' => 100,
+					),
+				),
+				'selectors'  => array(
+					'{{WRAPPER}}.elementor-wc-products  ul.products' => 'grid-column-gap: {{SIZE}}{{UNIT}}',
+				),
+			)
+		);
+
+		$this->add_control(
+			'row_gap',
+			array(
+				'label'      => esc_html__( 'Rows Gap', 'mas-elementor' ),
+				'type'       => Controls_Manager::SLIDER,
+				'size_units' => array( 'px', 'em', 'rem', 'custom' ),
+				'default'    => array(
+					'size' => 40,
+				),
+				'range'      => array(
+					'px' => array(
+						'min' => 0,
+						'max' => 100,
+					),
+				),
+				'selectors'  => array(
+					'{{WRAPPER}}.elementor-wc-products  ul.products' => 'grid-row-gap: {{SIZE}}{{UNIT}}',
+				),
+			)
+		);
+
+		$this->add_responsive_control(
+			'align',
+			array(
+				'label'        => esc_html__( 'Alignment', 'mas-elementor' ),
+				'type'         => Controls_Manager::CHOOSE,
+				'options'      => array(
+					'left'   => array(
+						'title' => esc_html__( 'Left', 'mas-elementor' ),
+						'icon'  => 'eicon-text-align-left',
+					),
+					'center' => array(
+						'title' => esc_html__( 'Center', 'mas-elementor' ),
+						'icon'  => 'eicon-text-align-center',
+					),
+					'right'  => array(
+						'title' => esc_html__( 'Right', 'mas-elementor' ),
+						'icon'  => 'eicon-text-align-right',
+					),
+				),
+				'prefix_class' => 'elementor-product-loop-item--align-',
+				'selectors'    => array(
+					'{{WRAPPER}} .product' => 'text-align: {{VALUE}}',
+				),
+			)
+		);
+
+		$this->add_control(
+			'heading_image_style',
+			array(
+				'label'     => esc_html__( 'Image', 'mas-elementor' ),
+				'type'      => Controls_Manager::HEADING,
+				'separator' => 'before',
+			)
+		);
+
+		$this->add_group_control(
+			Group_Control_Border::get_type(),
+			array(
+				'name'     => 'image_border',
+				'selector' => '{{WRAPPER}} a > img',
+			)
+		);
+
+		$this->add_responsive_control(
+			'image_border_radius',
+			array(
+				'label'      => esc_html__( 'Border Radius', 'mas-elementor' ),
+				'type'       => Controls_Manager::DIMENSIONS,
+				'size_units' => array( 'px', '%', 'em', 'rem', 'custom' ),
+				'selectors'  => array(
+					'{{WRAPPER}} a > img' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}}',
+				),
+			)
+		);
+
+		$this->add_responsive_control(
+			'image_spacing',
+			array(
+				'label'      => esc_html__( 'Spacing', 'mas-elementor' ),
+				'type'       => Controls_Manager::SLIDER,
+				'size_units' => array( 'px', 'em', 'rem', 'custom' ),
+				'selectors'  => array(
+					'{{WRAPPER}} a > img' => 'margin-bottom: {{SIZE}}{{UNIT}}',
+				),
+			)
+		);
+
+		$this->add_control(
+			'heading_title_style',
+			array(
+				'label'     => esc_html__( 'Title', 'mas-elementor' ),
+				'type'      => Controls_Manager::HEADING,
+				'separator' => 'before',
+			)
+		);
+
+		$this->add_control(
+			'title_color',
+			array(
+				'label'     => esc_html__( 'Color', 'mas-elementor' ),
+				'type'      => Controls_Manager::COLOR,
+				'global'    => array(
+					'default' => Global_Colors::COLOR_PRIMARY,
+				),
+				'selectors' => array(
+					'{{WRAPPER}} .woocommerce .woocommerce-loop-category__title' => 'color: {{VALUE}}',
+				),
+			)
+		);
+
+		$this->add_group_control(
+			Group_Control_Typography::get_type(),
+			array(
+				'name'     => 'title_typography',
+				'global'   => array(
+					'default' => Global_Typography::TYPOGRAPHY_PRIMARY,
+				),
+				'selector' => '{{WRAPPER}} .woocommerce .woocommerce-loop-category__title',
+			)
+		);
+
+		$this->add_control(
+			'heading_count_style',
+			array(
+				'label'     => esc_html__( 'Count', 'mas-elementor' ),
+				'type'      => Controls_Manager::HEADING,
+				'separator' => 'before',
+			)
+		);
+
+		$this->add_control(
+			'count_color',
+			array(
+				'label'     => esc_html__( 'Color', 'mas-elementor' ),
+				'type'      => Controls_Manager::COLOR,
+				'selectors' => array(
+					'{{WRAPPER}} .woocommerce-loop-category__title .count' => 'color: {{VALUE}}',
+				),
+			)
+		);
+
+		$this->add_group_control(
+			Group_Control_Typography::get_type(),
+			array(
+				'name'     => 'count_typography',
+				'global'   => array(
+					'default' => Global_Typography::TYPOGRAPHY_PRIMARY,
+				),
+				'selector' => '{{WRAPPER}} .woocommerce-loop-category__title .count',
+			)
+		);
+
+		$this->end_controls_section();
+	}
+
+	/**
+	 * Get ShortCode.
+	 */
+	private function get_shortcode() {
+		$settings = $this->get_settings();
+
+		$attributes = array(
+			'number'     => $settings['number'],
+			'columns'    => $settings['columns'],
+			'hide_empty' => ( 'yes' === $settings['hide_empty'] ) ? 1 : 0,
+			'orderby'    => $settings['orderby'],
+			'order'      => $settings['order'],
+		);
+
+		if ( 'by_id' === $settings['source'] ) {
+			$attributes['ids'] = implode( ',', $settings['categories'] );
+		} elseif ( 'by_parent' === $settings['source'] ) {
+			$attributes['parent'] = $settings['parent'];
+		} elseif ( 'current_subcategories' === $settings['source'] ) {
+			$attributes['parent'] = get_queried_object_id();
+		}
+
+		$this->add_render_attribute( 'shortcode', $attributes );
+
+		$shortcode = sprintf( '[product_categories %s]', $this->get_render_attribute_string( 'shortcode' ) );
+
+		return $shortcode;
+	}
+
+	/**
+	 * Render.
+	 */
+	public function render() {
+		$product_categories_html = do_shortcode( $this->get_shortcode() );
+
+		if ( $product_categories_html ) {
+			$product_categories_html = str_replace( '<ul class="products', '<ul class="products mas-grid', $product_categories_html );
+
+			// PHPCS - Doesn't need to be escaped since it's a WooCommerce template, and 3rd party plugins might hook into it.
+			echo $product_categories_html; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		}
+	}
+
+	/**
+	 * Render Plain Content.
+	 */
+	public function render_plain_content() {
+		echo wp_kses_post( $this->get_shortcode() );
+	}
+
+	/**
+	 * Get group name.
+	 */
+	public function get_group_name() {
+		return 'woocommerce';
+	}
+}
