1dba5407a324ea1de8b92ec1f837e26b6c5ad5e5
YahyaTec <yahya.techy@gmail.com>
2023-07-12 17:39:16 +0530

    added product stock and title

---
+++ modules/woocommerce/widgets/product-stock.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * The Product Stock Widget.
+ *
+ * @package MASElementor/Modules/Woocommerce/Widgets
+ */
+
+namespace MASElementor\Modules\Woocommerce\Widgets;
+
+use Elementor\Controls_Manager;
+use Elementor\Group_Control_Typography;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Class Product_Stock
+ */
+class Product_Stock extends Base_Widget {
+
+	/**
+	 * Get the name of the widget.
+	 *
+	 * @return string
+	 */
+	public function get_name() {
+		return 'mas-woocommerce-product-stock';
+	}
+
+	/**
+	 * Get the title of the widget.
+	 *
+	 * @return string
+	 */
+	public function get_title() {
+		return esc_html__( 'MAS Product Stock', 'mas-elementor' );
+	}
+
+	/**
+	 * Get the icon of the widget.
+	 *
+	 * @return string
+	 */
+	public function get_icon() {
+		return 'eicon-product-stock';
+	}
+
+	/**
+	 * Get the keywords related to the widget.
+	 *
+	 * @return array
+	 */
+	public function get_keywords() {
+		return array( 'woocommerce', 'shop', 'store', 'stock', 'quantity', 'product' );
+	}
+
+	/**
+	 * Register controls for this widget.
+	 */
+	protected function register_controls() {
+
+		$this->start_controls_section(
+			'section_product_stock_style',
+			array(
+				'label' => esc_html__( 'Style', 'mas-elementor' ),
+				'tab'   => Controls_Manager::TAB_STYLE,
+			)
+		);
+
+		$this->add_control(
+			'wc_style_warning',
+			array(
+				'type'            => Controls_Manager::RAW_HTML,
+				'raw'             => esc_html__( 'The style of this widget is often affected by your theme and plugins. If you experience any such issue, try to switch to a basic theme and deactivate related plugins.', 'mas-elementor' ),
+				'content_classes' => 'elementor-panel-alert elementor-panel-alert-info',
+			)
+		);
+
+		$this->add_control(
+			'text_color',
+			array(
+				'label'     => esc_html__( 'Text Color', 'mas-elementor' ),
+				'type'      => Controls_Manager::COLOR,
+				'selectors' => array(
+					'.woocommerce {{WRAPPER}} .stock' => 'color: {{VALUE}}',
+				),
+			)
+		);
+
+		$this->add_group_control(
+			Group_Control_Typography::get_type(),
+			array(
+				'name'     => 'text_typography',
+				'selector' => '.woocommerce {{WRAPPER}} .stock',
+			)
+		);
+
+		$this->end_controls_section();
+	}
+
+	/**
+	 * Render.
+	 */
+	protected function render() {
+		global $product;
+		$product = $this->get_product();
+
+		if ( ! $product ) {
+			return;
+		}
+
+		// PHPCS - the method wc_get_stock_html is safe.
+		echo wc_get_stock_html( $product ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	}
+
+	/**
+	 * Render Plain Content.
+	 */
+	public function render_plain_content() {}
+
+	/**
+	 * Get group name.
+	 */
+	public function get_group_name() {
+		return 'woocommerce';
+	}
+}
