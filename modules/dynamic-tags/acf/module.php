e86ad53bef97a171c544580536c356658dcaa781
YahyaTec <yahya.techy@gmail.com>
2024-02-09 17:13:47 +0530

    #74 added acf file, url dynamic tag

--- modules/dynamic-tags/acf/module.php
+++ modules/dynamic-tags/acf/module.php
@@ -133,6 +133,8 @@ class Module extends DynamicTags\Module {
 		return array(
 			'ACF_Image',
 			'ACF_Text',
+			'ACF_File',
+			'ACF_URL',
 		);
 	}
 
@@ -176,7 +178,7 @@ class Module extends DynamicTags\Module {
 	 */
 	public static function get_text_tag_value_field( Base_Tag $tag ) {
 		$key = $tag->get_settings( 'key' );
-		if ( ! empty( $key ) && !(int) $key ) {
+		if ( ! empty( $key ) && ! (int) $key ) {
 			list( $field_key, $meta_key ) = explode( ':', $key );
 			return array( self::get_acf_field( $field_key, $meta_key ), $meta_key );
 		}
@@ -194,7 +196,7 @@ class Module extends DynamicTags\Module {
 	public static function get_user_tag_value_field( Base_Tag $tag, $author_id ) {
 		$key = $tag->get_settings( 'key' );
 
-		if ( ! empty( $key ) && !(int) $key ) {
+		if ( ! empty( $key ) && ! (int) $key ) {
 			list( $field_key, $meta_key ) = explode( ':', $key );
 			return array( self::get_acf_field( $field_key, $meta_key, 'user_' . $author_id ), $meta_key );
 		}
---
+++ modules/dynamic-tags/acf/tags/acf-file.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * ACF Dynamic File tag .
+ *
+ * @package MASElementor\Modules\DynamicTags\ACF\Tags
+ */
+
+namespace MASElementor\Modules\DynamicTags\ACF\Tags;
+
+use MASElementor\Modules\DynamicTags\ACF\Module;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * ACF File Class - Dynamic.
+ */
+class ACF_File extends ACF_Image {
+
+	/**
+	 * Get the name of dynamic  acf-file.
+	 */
+	public function get_name() {
+		return 'acf-file';
+	}
+
+	/**
+	 * Get the title of dynamic acf-file.
+	 */
+	public function get_title() {
+		return esc_html__( 'ACF', 'mas-elementor' ) . ' ' . esc_html__( 'File Field', 'mas-elementor' );
+	}
+
+	/**
+	 * Get the categories of dynamic acf-file.
+	 */
+	public function get_categories() {
+		return array(
+			Module::MEDIA_CATEGORY,
+		);
+	}
+
+	/**
+	 * Get supported fields.
+	 */
+	public function get_supported_fields() {
+		return array(
+			'file',
+		);
+	}
+}
